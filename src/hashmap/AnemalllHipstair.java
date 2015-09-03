1d7f6d6fcd783aca1830ccebd63fbb5ed5e4ca67
Kendra Kleber <Guest@wts11.attlocal.net>
2015-09-02 18:26:28 -0700

    no one likes hip stairs anywheys

---
+++ src/hashmap/AnemalllHipstair.java
@@ -0,0 +1,19 @@
+package hashmap;
+
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AnemalllHipstair {
+	private AbstractCollection<String> findAnimalHipsters(
+			HashMap<String, ArrayList<String>> network,
+			HashMap<String, String> favoriteAnimals) {
+		
+		// Person  favorite amial, perons friends, compare animal to friends animal
+		
+		for(String s: network) {
+			
+		}
+		return null;
+	}
+}
