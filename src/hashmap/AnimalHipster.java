f3d7c3e9e5f90740cb6a837cd8a5d22577387d60
Kendra Kleber <Guest@wts11.attlocal.net>
2015-09-09 18:25:11 -0700

    people stared at hips
    and we exceeded the google chat box limit

--- src/hashmap/AnimalHipster.java
+++ src/hashmap/AnimalHipster.java
@@ -11,32 +11,71 @@ import java.util.HashMap;
 
 import org.junit.Test;
 
-public class AnimalHipster
-{
+public class AnimalHipster {
 	@Test
-	public void testAnimalHipster()
-	{
+	public void testAnimalHipster() {
 		AnimalHipster animalHipster = new AnimalHipster();
 
-		HashMap<String, ArrayList<String>> network = new HashMap<String, ArrayList<String>>();
+		HashMap<String, ArrayList<String>> friendNetwork = new HashMap<String, ArrayList<String>>();
 		HashMap<String, String> favoriteAnimals = new HashMap<String, String>();
-		
-		network.put("Amy", new ArrayList<String>(Arrays.asList(new String[] {"Brie", "Chris"})));
-		network.put("Brie", new ArrayList<String>(Arrays.asList(new String[] {"Amy", "Chris", "Elena", "Devney"})));
-		network.put("Chris", new ArrayList<String>(Arrays.asList(new String[] {"Amy", "Brie", "Elena", "Fan-Hal"})));
-		network.put("Devney", new ArrayList<String>(Arrays.asList(new String[] {"Brie", "Elena"})));
-		network.put("Elena", new ArrayList<String>(Arrays.asList(new String[] {"Brie", "Chris", "Devney", "Fan-Hal"})));
-		network.put("Fan-Hal", new ArrayList<String>(Arrays.asList(new String[] {"Chris", "Elena"})));
-		
+
+		friendNetwork.put(
+				"Amy",
+				new ArrayList<String>(Arrays.asList(new String[] { "Brie",
+						"Chris" })));
+		friendNetwork.put(
+				"Brie",
+				new ArrayList<String>(Arrays.asList(new String[] { "Amy",
+						"Chris", "Elena", "Devney" })));
+		friendNetwork.put(
+				"Chris",
+				new ArrayList<String>(Arrays.asList(new String[] { "Amy",
+						"Brie", "Elena", "Fan-Hal" })));
+		friendNetwork.put(
+				"Devney",
+				new ArrayList<String>(Arrays.asList(new String[] { "Brie",
+						"Elena" })));
+		friendNetwork.put(
+				"Elena",
+				new ArrayList<String>(Arrays.asList(new String[] { "Brie",
+						"Chris", "Devney", "Fan-Hal" })));
+		friendNetwork.put(
+				"Fan-Hal",
+				new ArrayList<String>(Arrays.asList(new String[] { "Chris",
+						"Elena" })));
+
 		favoriteAnimals.put("Amy", "Quokka");
 		favoriteAnimals.put("Devney", "Springbok");
 		favoriteAnimals.put("Brie", "Springbok");
 		favoriteAnimals.put("Chris", "Nubian Ibex");
 		favoriteAnimals.put("Elena", "Quokka");
 		favoriteAnimals.put("Fan-Hal", "Springbok");
-		
-		assertTrue(animalHipster.findAnimalHipsters(network, favoriteAnimals).containsAll(new ArrayList<String>(Arrays.asList(new String[] {"Amy", "Elena", "Chris", "Fan-Hal"}))));
+
+		assertTrue(animalHipster.findAnimalHipsters(friendNetwork,
+				favoriteAnimals).containsAll(
+				new ArrayList<String>(Arrays.asList(new String[] { "Amy",
+						"Elena", "Chris", "Fan-Hal" }))));
 	}
 
-	
+	private AbstractCollection<String> findAnimalHipsters(
+			HashMap<String, ArrayList<String>> friendNetwork,
+			HashMap<String, String> favoriteAnimals) {
+		ArrayList<String> hipstairers = new ArrayList<String>();
+		hipstairers.addAll(favoriteAnimals.keySet());
+		for (ArrayList<String> s : friendNetwork.values()) {
+			System.out.println();
+			String favAnimal = favoriteAnimals.get(friendNetwork.get(s));
+			for (String f : s) {
+				if (favAnimal == favoriteAnimals.get(f)) {
+					System.out.println(friendNetwork.get(s)
+							+ " is not an animal hipstarer because of "
+							+ friendNetwork.get(f));
+					hipstairers.remove(s);
+				}
+			}
+
+		}
+		return hipstairers;
+	}
+
 }
