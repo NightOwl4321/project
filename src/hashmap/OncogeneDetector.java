1b513177458160d9e0af95fa5941daec954447a7
Kendra Kleber <Guest@wts11.attlocal.net>
2015-09-16 18:29:31 -0700

    more checks

--- src/hashmap/OncogeneDetector.java
+++ src/hashmap/OncogeneDetector.java
@@ -1,5 +1,13 @@
 package hashmap;
 
+import java.util.ArrayList;
+
 public class OncogeneDetector {
+
+	public boolean isOncogene(ArrayList<String> healthySequences,
+			ArrayList<String> cancerSequences, String string) {
+		
+		return false;
+	}
 
 }
